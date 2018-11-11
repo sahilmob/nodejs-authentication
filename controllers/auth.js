4185dca9600cacac3433a0517f9518d28c62a2db
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-11 22:09:03 +0200

    add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,14 +18,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	User.findById("5be6dc02acfe2821a87bd7e2")
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({ email })
 		.then(user => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save(err => {
-				console.log(err);
-				res.redirect("/");
-			});
+			if (!user) {
+				return res.redirect("/login");
+			}
+			bcrypt
+				.compare(password, user.password)
+				.then(doMatch => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save(err => {
+							console.log(err);
+							res.redirect("/");
+						});
+					}
+					res.redirect("/login");
+				})
+				.catch(err => {
+					console.log(err);
+					res.redirect("/logout");
+				});
 		})
 		.catch(err => console.log(err));
 };
